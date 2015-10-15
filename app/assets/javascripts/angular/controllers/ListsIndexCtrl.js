4806db3cd9d39200f562eb7897155cb8fc4382d9
Dan Berger <dsberger@gmail.com>
2015-10-15 16:44:00 -0700

    sorts out separate card and list drag and drop zones

--- app/assets/javascripts/angular/controllers/ListsIndexCtrl.js
+++ app/assets/javascripts/angular/controllers/ListsIndexCtrl.js
@@ -6,15 +6,24 @@ cardShuffler.controller('ListsIndexCtrl',
 
         $scope.lists = [];
 
-        $scope.onListDropComplete = function( moveTo, list ){
-          rearrangeList( moveTo, list );
-          ListOrderAPI.update( list.id, moveTo + 1 )
-            .then( function(response){
-              $scope.lists = response;
-              $scope.lists.sort(function(a,b){
-                return parseInt(a.order_on_board) - parseInt(b.order_on_board)
+        $scope.onListDropComplete = function( moveToList, object ){
+          if (object.type === "list") {
+            rearrangeList( moveToList, object );
+            ListOrderAPI.update( object.id, moveToList + 1 )
+              .then( function(response){
+                $scope.lists = response;
+                $scope.lists.sort(function(a,b){
+                  return parseInt(a.order_on_board) - parseInt(b.order_on_board)
+                });
               });
-            });
+          };
+        };
+
+        $scope.onCardDropComplete = function( moveToList, moveToCard, object ){
+          if ( object.type === "card" ) {
+            console.log( "Moving to list " + moveToList + " and card " + moveToCard );
+            console.log( object );
+          };
         };
 
         var rearrangeList = function( moveTo, list ){
--- app/views/lists/index.json.jbuilder
+++ app/views/lists/index.json.jbuilder
@@ -1,9 +1,11 @@
 json.array! @lists do |list|
+  json.type "list"
   json.id list.id
   json.name list.name
   json.color list.color
   json.order_on_board list.order_on_board
   json.cards list.cards do |card|
+    json.type "card"
     json.id card.id
     json.name card.name
     json.order_on_list card.order_on_list
