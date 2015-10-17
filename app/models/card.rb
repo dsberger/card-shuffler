67eaa41297b9fc2a9e53341fa427f7f5f7a57b39
Dan Berger <dsberger@gmail.com>
2015-10-16 22:23:42 -0700

    majorly refactors Card.move

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,43 +3,110 @@ class Card < ActiveRecord::Base
 
   validates :name, :list_id, :color, presence: true
 
+
   def position
     list.name + order_on_list.to_s
   end
 
-  def self.move( card_id, new_list, to_card )
+
+  def self.move( card_id, to_list, to_card )
+
     moving_card = Card.find card_id
-    from_card = moving_card.order_on_list
-    
-    if from_card < to_card
-      Card.move_up( moving_card.list.id, from_card, to_card )
+    new_list = List.find_by_order_on_board(to_list)
+
+    if new_list == moving_card.list
+
+      Card.move_within_list(
+        moving_card,
+        new_list,
+        to_card )
+
     else
-      Card.move_down( moving_card.list.id, from_card, to_card )
+
+      Card.move_across_lists(
+        moving_card,
+        new_list,
+        to_card
+      )
+
     end
 
-    moving_card.order_on_list = to_card
-    moving_card.save
   end
+
 
   private
 
-  def self.move_up(list_id, from, to)
-    displaced = List.find(list_id).cards
-      .where(order_on_list: (from + 1..to))
 
-    displaced.each do |card|
-      card.order_on_list -= 1
-      card.save
+  def self.move_within_list( card, list, new_order_on_list )
+
+    old_order_on_list = card.order_on_list
+
+    if old_order_on_list < new_order_on_list
+
+      Card.iterate_order_down(
+        list.id,
+        old_order_on_list + 1,
+        new_order_on_list
+      )
+
+    else
+
+      Card.iterate_order_up(
+        list.id,
+        new_order_on_list,
+        old_order_on_list - 1
+      )
+
     end
+
+    card.update(
+      { order_on_list: new_order_on_list }
+    )
+
   end
 
-  def self.move_down(list_id, from, to)
+
+  def self.move_across_lists(card, new_list, new_order_on_list)
+
+    Card.iterate_order_down(
+      card.list.id,
+      card.order_on_list + 1,
+      card.list.cards.count
+    )
+
+    Card.iterate_order_up(
+      new_list.id,
+      new_order_on_list,
+      new_list.cards.count
+    )
+
+    card.update(
+      { list: new_list,
+        order_on_list: new_order_on_list }
+    )
+
+  end
+
+
+  def self.iterate_order_down(list_id, min, max)
+    Card.iterate_group(list_id, min, max, -1)
+  end
+
+
+  def self.iterate_order_up(list_id, min, max)
+    Card.iterate_group(list_id, min, max, 1)
+  end
+
+
+  def self.iterate_group(list_id, min, max, amount)
+
     displaced = List.find(list_id).cards
-      .where(order_on_list: (to.. from - 1))
-    
+      .where(order_on_list: (min..max))
+
     displaced.each do |card|
-      card.order_on_list += 1
+      card.order_on_list += amount
       card.save
     end
+
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -148,6 +148,41 @@ describe Card do
     end
     
     describe "to another list" do
+
+      before do
+        list_in = List.find_by_name("list3") 
+        card = Card.find_by_name("card7")
+        Card.move( card.id, list_in.id, 4 )
+      end
+
+      it "shortens the list it's leaving by one" do
+        expect( List.find_by_name("list2").cards.count).to eq 4
+      end
+
+      it "iterates down all the cards that follow it in the list it's leaving" do
+        expect( Card.find_by_name("card8").order_on_list).to eq 2
+        expect( Card.find_by_name("card9").order_on_list).to eq 3
+        expect( Card.find_by_name("card10").order_on_list).to eq 4
+      end
+
+      it "leaves the cards that precede it on the list it's leaving untouched" do
+        expect( Card.find_by_name("card6").order_on_list).to eq 1
+      end
+
+      it "lengthens the list it's inserted into by one" do
+        expect( List.find_by_name("list3").cards.count).to eq 6
+      end
+
+      it "iterates up all the cards after it on the list it's inserted into" do
+        expect( Card.find_by_name("card14").order_on_list).to eq 5
+        expect( Card.find_by_name("card15").order_on_list).to eq 6
+      end
+
+      it "leaves the cards that precede it in the list it's inserted into untouched" do
+        expect( Card.find_by_name("card11").order_on_list).to eq 1
+        expect( Card.find_by_name("card12").order_on_list).to eq 2
+        expect( Card.find_by_name("card13").order_on_list).to eq 3
+      end
     end
   end
 end
