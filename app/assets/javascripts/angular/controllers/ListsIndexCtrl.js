0f1e96afedaff4fd2258b2142439dec7eecae73c
Dan Berger <dsberger@gmail.com>
2015-10-27 15:58:44 -0700

    refactors a bunch of list and card movement functions

--- app/assets/javascripts/angular/controllers/ListsIndexCtrl.js
+++ app/assets/javascripts/angular/controllers/ListsIndexCtrl.js
@@ -7,124 +7,82 @@ cardShuffler.controller('ListsIndexCtrl',
 
         $scope.lists = [];
 
+        $scope.saveCardAndListIndex = function( cardIndex, listIndex ){
+          saveCardIndex( cardIndex );
+          $scope.saveListIndex( listIndex );
+        };
+
         $scope.onListDropComplete = function( moveToList, object ){
           if (object.type === "list") {
-            rearrangeLists( moveToList, object );
+            rearrangeLists( moveToList );
             ListOrderAPI.update( object.id, moveToList + 1 )
               .then( function(response){
                 $scope.lists = response;
-                $scope.lists.sort(function(a,b){
-                  return parseInt(a.order_on_board) - parseInt(b.order_on_board)
-                });
-                $scope.lists.forEach( function( list ){
-                  list.cards.sort(function(a,b){
-                    return parseInt(a.order_on_list) - parseInt(b.order_on_list);
-                  });
-                });
+                sortListsAndCards();
               });
           };
         };
 
         $scope.onCardDropComplete = function( moveToList, moveToCard, object ){
           if ( object.type === "card" ) {
-            rearrangeCards( moveToList, moveToCard, object );
+            rearrangeCards( moveToList, moveToCard );
             CardOrderAPI.update( object.id, moveToList + 1, moveToCard + 1 )
               .then( function(response){
                 $scope.lists = response;
-                $scope.lists.sort(function(a,b){
-                  return parseInt(a.order_on_board) - parseInt(b.order_on_board)
-                });
-                $scope.lists.forEach( function( list ){
-                  list.cards.sort(function(a,b){
-                    return parseInt(a.order_on_list) - parseInt(b.order_on_list);
-                  });
-                });
+                sortListsAndCards();
               });
           };
         };
 
-        $scope.saveCardAndListIndex = function( cardIndex, listIndex ){
-
-          if ($scope.originatingCard === undefined ){
-            $scope.originatingCard = cardIndex;
-          };
-
-          if ($scope.originatingList === undefined ){
-            $scope.originatingList = listIndex;
-          };
-
-        };
-
-        function rearrangeLists( moveTo, list ) {
-          var moveFrom = $scope.lists.indexOf( list );
-          $scope.lists[moveFrom] = null;
-
-          if ( moveFrom > moveTo ) {
-            iterateDownLists( moveFrom, moveTo );
-          } else {
-            iterateUpLists( moveFrom, moveTo );
-          }
-
-          $scope.lists[moveFrom] = list;
-        };
-
-        function iterateDownLists( moveFrom, moveTo ) {
-          var i = moveFrom;
-          for ( i; i > moveTo; i-- ){
-            var movingList = $scope.lists[i-1];
-            $scope.lists[i-1] = null;
-            $scope.lists[i] = movingList;
+        $scope.saveListIndex = function( listIndex ) {
+          if ($scope.savedListIndex === undefined ){
+            $scope.savedListIndex = listIndex;
           };
         };
 
-        function iterateUpLists( moveFrom, moveTo ) {
-          var i = moveFrom;
-          for ( i; i < moveTo; i++){
-            var movingList = $scope.lists[i+1];
-            $scope.lists[i+1] = null;
-            $scope.lists[i] = movingList;
+        function oldListIndex(){
+          var index = $scope.savedListIndex;
+          $scope.savedListIndex = undefined;
+          return index;
+        };
+
+        function saveCardIndex( cardIndex ) {
+          if ($scope.savedCardIndex === undefined ){
+            $scope.savedCardIndex = cardIndex;
           };
+        };
+
+        function oldCardIndex(){
+          var index = $scope.savedCardIndex;
+          $scope.savedCardIndex = undefined;
+          return index;
+        };
+
+        function rearrangeLists( newListIndex ) {
+          var list = $scope.lists.splice( oldListIndex(), 1 )[0];
+          $scope.lists.splice( newListIndex, 0, list );
         };
         
-        function retrieveOriginatingList(){
-          var list = $scope.originatingList;
-          $scope.originatingList = undefined;
-          return list;
-        };
-
-        function retrieveOriginatingCard(){
-          var card = $scope.originatingCard;
-          $scope.originatingCard = undefined;
-          return card;
-        };
-
         function rearrangeCards( newListIndex, newCardIndex ){
-          var oldList = retrieveOriginatingList();
-          var oldCard = retrieveOriginatingCard();
-          var card = pullCardAt( oldList, oldCard );
-          insertCardTo( newListIndex, newCardIndex, card );
+          var card = $scope.lists[ oldListIndex() ].cards.splice( oldCardIndex(), 1 )[0];
+          $scope.lists[ newListIndex ].cards.splice( newCardIndex, 0, card );
         };
 
-        function pullCardAt( listIndex, cardIndex ) {
-          return $scope.lists[ listIndex ].cards.splice( cardIndex, 1 )[0];
+        function sortListsAndCards() {
+          $scope.lists.sort(function(a,b){
+            return parseInt(a.order_on_board) - parseInt(b.order_on_board)
+          });
+          $scope.lists.forEach( function( list ){
+            list.cards.sort(function(a,b){
+              return parseInt(a.order_on_list) - parseInt(b.order_on_list);
+            });
+          });
         };
-
-        function insertCardTo( listIndex, cardIndex, card) {
-          $scope.lists[ listIndex ].cards.splice( cardIndex, 0, card );
-        };
-
 
         ListsAPI.index()
           .then( function( listsResponse ){
             $scope.lists.push.apply( $scope.lists, listsResponse );
-            $scope.lists.sort(function(a,b){
-              return parseInt(a.order_on_board) - parseInt(b.order_on_board)
-            });
-            $scope.lists.forEach( function( list ){
-              list.cards.sort(function(a,b){
-                return parseInt(a.order_on_list) - parseInt(b.order_on_list);
-              });
-            });
+            sortListsAndCards();
           });
 
       }]);
